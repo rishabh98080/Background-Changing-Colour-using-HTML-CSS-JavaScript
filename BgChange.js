fbf9dc47cee8b8b8812372559281baa9a3115160
rishabh98080 <rishabh24273239pandey@gmail.com>
2024-11-03 21:34:09 +0530

    Add files via upload

---
+++ BgChange.js
@@ -0,0 +1,9 @@
+document.getElementById("Red").onclick = function(){
+    document.body.style.background = "Red";
+    document.getElementById("div1").style.background = "coral";
+}
+document.getElementById("Green").onclick = () => {document.body.style.background = "Green";document.getElementById("div1").style.background = "lightgreen"};
+document.getElementById("Yellow").onclick = () => {document.body.style.background = "Yellow";document.getElementById("div1").style.background = "lightyellow"};
+document.getElementById("Pink").onclick = () => {document.body.style.background = "Pink";document.getElementById("div1").style.background = "lightpink"};
+document.getElementById("Blue").onclick = () => {document.body.style.background = "Blue";document.getElementById("div1").style.background = "lightblue"};
+
